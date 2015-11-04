edc4a55ca331acaa9621525a76e6a15ac6ff5994
Chanon <chanon.s@gmail.com>
2015-11-05 01:05:40 +0700

    fixed to use typings property

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var file_module = require('./file_module');
 var index_module = require('index_module');
 var package_module = require('package_module');
-var package_module2 = require('package_module2');
 file_module.runMe();
 index_module.runMe();
 package_module.runMe();
-package_module2.runMe();
--- app.ts
+++ app.ts
@@ -1,9 +1,7 @@
 import * as file_module from './file_module';
 import * as index_module from 'index_module';
 import * as package_module from 'package_module';
-import * as package_module2 from 'package_module2';
 
 file_module.runMe();
 index_module.runMe();
 package_module.runMe();
-package_module2.runMe();
