4736c5097f799b2b5efd072a44b3d786333a6a90
Chanon <chanon.s@gmail.com>
2015-11-04 23:21:42 +0700

    try es6 style imports

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
-import file_module = require('./file_module');
-import index_module = require('index_module');
-import package_module = require('package_module');
+import * as file_module from './file_module';
+import * as index_module from 'index_module';
+import * as package_module from 'package_module';
 
 file_module.runMe();
 index_module.runMe();
