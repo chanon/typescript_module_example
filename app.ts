c9b2c1293ba4aa32880c312466fd875a893859d6
Chanon <chanon.s@gmail.com>
2015-11-05 00:23:36 +0700

    try using typings declaration

--- app.ts
+++ app.ts
@@ -1,7 +1,9 @@
 import * as file_module from './file_module';
 import * as index_module from 'index_module';
 import * as package_module from 'package_module';
+import * as package_module2 from 'package_module2';
 
 file_module.runMe();
 index_module.runMe();
 package_module.runMe();
+package_module2.runMe();
